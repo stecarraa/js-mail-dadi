afef935c08f74a49429c1e9dd11bfdc6cab6a2ed
stecarraa <stefano@caratozzolo.me>
2022-06-17 16:11:22 +0200

    mail exercise ook

--- Email/mai..js
+++
@@ -1,8 +0,0 @@
-// Mail
-// Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
-
-// creo un array dentro al quale ci scrivo alcune mail
-
-const maillist = ['stefano@caratozzolo.me', 'giulia@cartozzolo.me', 'marisella@caratozzolo.me', 'francesco@caratozzolo.me']
-
-// adesso attraverso un promt chiedo all'utente di inserire una mail che sarà quella che poi devo controllare
---
+++ Email/main.js
@@ -0,0 +1,36 @@
+// Mail
+// Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+
+// creo un array dentro al quale ci scrivo alcune mail
+
+const mailList = ['stefano@caratozzolo.me', 'giulia@cartozzolo.me', 'marisella@caratozzolo.me', 'francesco@caratozzolo.me']
+
+console.log(mailList)
+
+// adesso attraverso un promt chiedo all'utente di inserire una mail che sarà quella che poi devo controllare
+
+
+const userMail = prompt('Inserisci il tuo indirizzo email');
+
+// adesso partendi dal presupposto che la mail dentro non ci sia facciamo partire un ciclo
+// for che scorra tutti gli elementi dell' array fino a che non trova una mail corrispondente ed eventualmente 
+//ci restituisca il risultato contrario
+
+let mailExist = false
+
+for(let i = 0; i < mailList.length; i++){
+
+    if(mailList[i] === userMail){
+        mailExist = true
+
+    document.getElementById('message').innerHTML='La mail: ' + userMail + ' esiste già, registrati con un\'altra mail '
+
+    }
+    else{
+        document.getElementById('message').innerHTML='La mail: ' + userMail + ' non è stata trovata, puoi procedere con la registrazione '
+
+    }
+
+}
+
+console.log(mailExist);
